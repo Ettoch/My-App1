5c864bde8d902c98e85b2526c39b5157503dcd7b
Youssef <yousseftor2006@gmail.com>
2026-07-01 11:10:37 +0100

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,20 +8,12 @@ const SafeAreaView=styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-bold text-primary">
+        Home 
       </Text>
-      <Link href="my-app/app/onboarding" className="mt-4 rounded bg-[#081126] text-white p-4"><Text>Go To Onboarding</Text></Link> 
-      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-[#081126] text-white p-4"><Text>Go To sign-In</Text></Link>
-      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-[#081126] text-white p-4"><Text>Go To sign-Up</Text></Link>
-      
-      <Link href="/subscriptions/spotify"><Text>Spotify Subscription</Text ></Link>
-      <Link
-           href={{
-            pathname:"/subscriptions/[id]",
-            params:{id:"claude"}  
-          }}
-      ><Text>Claude Subscription</Text></Link>
+      <Link href="my-app/app/onboarding" className="mt-4 font-sans-extrabold rounded bg-[#081126] text-white p-4"><Text>Go To Onboarding</Text></Link> 
+      <Link href="/(auth)/sign-in" className="mt-4 font-sans-extrabold rounded bg-[#081126] text-white p-4"><Text>Go To sign-In</Text></Link>
+      <Link href="/(auth)/sign-up" className="mt-4 font-sans-extrabold rounded bg-[#081126] text-white p-4"><Text>Go To sign-Up</Text></Link>
     </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,37 @@
-import { Stack } from "expo-router";
+import { Stack,SplashScreen } from "expo-router";
 
 import "@/global.css"
+
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
 
 export const unstable_settings = {
   initialRouteName: "(tabs)",
 };
 
-export default function RootLayout() {
-  return <Stack screenOptions={{headerShown:false}} />;
+export default function Rootlayout(){
+  const [fonstLoaded]=useFonts({
+    'sans-regular':require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-bold':require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-semibold':require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'sans-extrabold':require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-light':require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+  })
+
+  useEffect(()=>{
+    if(fonstLoaded){
+      SplashScreen.hideAsync();
+    }
+  }, [fonstLoaded])
+
+  if(!fonstLoaded){
+    return null;
+  }
+
+  return(
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{headerShown:false}}/>
+    </Stack>
+  )
 }
