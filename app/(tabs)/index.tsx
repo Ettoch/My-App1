c79a1a67f8bb2e62f3bcd12bc9f0d27082c2ae9e
Youssef <yousseftor2006@gmail.com>
2026-07-09 18:04:25 +0100

    implement the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,94 @@
 import "../../global.css"
 import { styled } from "nativewind";
-import { SafeAreaView  as RNSafeAreaView} from "react-native-safe-area-context";
-import { Text, View} from "react-native";
-import {Link} from "expo-router"
-const SafeAreaView=styled(RNSafeAreaView);
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { Text, View, Image } from "react-native";
+import { Link } from "expo-router";
+import images  from "@/constants/images";
+import { HOME_USER,HOME_BALANCE,UPCOMING_SUBSCRIPTIONS, HOME_SUBSCRIPTIONS } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import ListHeading from "@/components/LIstHeading";
+import UpcomingSubscriptionCard from "@/components/UpcomingSubsriptionCard";
+import { FlatList } from "react-native";
+import SubscriptionCard from "@/components/subscriptioncard";
+import { useState } from "react";
+import { ScrollView } from "react-native";
+const UpcomingSubscriptionCardAny = UpcomingSubscriptionCard as any;
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
+  const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<string | null>(null);
+
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-5xl font-bold text-primary">
-        Home 
-      </Text>
-      <Link href="my-app/app/onboarding" className="mt-4 font-sans-extrabold rounded bg-[#081126] text-white p-4"><Text>Go To Onboarding</Text></Link> 
-      <Link href="/(auth)/sign-in" className="mt-4 font-sans-extrabold rounded bg-[#081126] text-white p-4"><Text>Go To sign-In</Text></Link>
-      <Link href="/(auth)/sign-up" className="mt-4 font-sans-extrabold rounded bg-[#081126] text-white p-4"><Text>Go To sign-Up</Text></Link>
+      <FlatList
+        data={HOME_SUBSCRIPTIONS}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SubscriptionCard
+            {...item}
+            expanded={expandedSubscriptionId === item.id}
+            onPress={() => {
+              setExpandedSubscriptionId((currentId) =>
+                currentId === item.id ? null : item.id
+              );
+            }}
+          />
+        )}
+        extraData={expandedSubscriptionId}
+        contentContainerStyle={{ gap: 12 }}
+        showsVerticalScrollIndicator={true}
+        ListEmptyComponent={
+          <Text className="home-empty-state">No subscriptions found</Text>
+        }
+        ListHeaderComponent={() => (
+          <>
+            <View className="home-header">
+              <View className="home-user">
+                <Image source={images.avatar} className="home-avatar" />
+                <Text className="home-user-name">{HOME_USER.name}</Text>
+              </View>
+
+              <Image source={icons.add} className="home-add-button" />
+            </View>
+
+            <View className="home-balance-card">
+              <Text className="home-balance-label">Balance</Text>
+              <View className="home-balance-row">
+                <Text className="home-balance-amount">
+                  {formatCurrency(HOME_BALANCE.amount)}
+                </Text>
+
+                <Text className="home-balance-date">
+                  {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD/YYYY")}
+                </Text>
+              </View>
+            </View>
+
+            <View>
+              <ListHeading title="Upcoming" />
+              <FlatList
+                data={UPCOMING_SUBSCRIPTIONS}
+                renderItem={({ item }) => (
+                  <UpcomingSubscriptionCardAny {...item} />
+                )}
+                contentContainerStyle={{ gap: 12 }}
+                keyExtractor={(item) => item.id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                ListEmptyComponent={
+                  <Text className="home-empty-state">
+                    No upcoming subscriptions
+                  </Text>
+                }
+              />
+            </View>
+
+            <ListHeading title="All Subscriptions" />
+          </>
+        )}
+      />
     </SafeAreaView>
   );
 }
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,19 +1,19 @@
 import activity from "../assets/images/claude.png";
-import add from "../assets/images/add.png";
-import adobe from "../assets/images/adobe.png";
+import add from "../assets/images/android-icon-foreground.png";
+import adobe from "../assets/images/figma.png";
 import back from "../assets/images/back.png";
 import claude from "../assets/images/activity.png";
 import dropbox from "../assets/images/dropbox.png";
-import figma from "../assets/images/figma.png";
-import github from "../assets/images/github.png";
+import figma from "../assets/images/menu.png";
+import github from "../assets/images/netflix.png";
 import home from "../assets/images/notion.png";
 import medium from "../assets/images/medium.png";
-import menu from "../assets/images/menu.png";
-import notion  from "../assets/images/home.png";
+import menu from "../assets/images/adobe.png";
+import notion  from "../assets/images/wallet.png";
 import openai from "../assets/images/openai.png";
 import plus from "../assets/images/plus.png";
 import setting from "../assets/images/android-icon-monochrome.png";
-import spotify from "../assets/images/spotify.png";
+import spotify from "../assets/images/avatar.png";
 import wallet from "../assets/images/expo-badge-white.png";
 
 export const icons = {
